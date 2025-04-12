3c3b2c716ee15228d53b72fdd82d901378ee1d40
1012ayush <sumitkatiyar783@gmail.com>
2025-04-12 21:33:02 +0530

    Create ProductData.js

---
+++ ProductData.js
@@ -0,0 +1,8 @@
+export const plants = [
+  { id: 1, name: "Fiddle Leaf", price: 20, image: "/1.jpg", category: "Indoor" },
+  { id: 2, name: "Snake Plant", price: 15, image: "/2.jpg", category: "Indoor" },
+  { id: 3, name: "Aloe Vera", price: 10, image: "/3.jpg", category: "Medicinal" },
+  { id: 4, name: "Spider Plant", price: 12, image: "/4.jpg", category: "Hanging" },
+  { id: 5, name: "Peace Lily", price: 18, image: "/5.jpg", category: "Indoor" },
+  { id: 6, name: "Lavender", price: 25, image: "/6.jpg", category: "Outdoor" }
+];
